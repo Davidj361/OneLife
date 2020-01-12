699a3ff68a4b7f9fc5d68722a8b6949ea2b43ddb
Davidj361 <david.j.361@gmail.com>
2020-01-12 13:24:19 -0500

    First edit from Davidj361

--- scripts/hetuwScripts/hetuwPushToHetuw.sh
+++ scripts/hetuwScripts/hetuwPushToHetuw.sh
@@ -5,12 +5,12 @@ cd OneLife
 echo 
 echo "PUSH OneLife"
 echo
-git push https://github.com/hetuw/OneLife
+git push https://github.com/Davidj361/OneLife
 cd ..
 cd minorGems
 echo 
 echo "PUSH minorGems"
 echo
-git push https://github.com/hetuw/minorGems
+git push https://github.com/Davidj361/minorGems
 cd ..
 cd ..
